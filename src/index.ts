6ede0ac8db65f2ee9f4af44738d39b16809ee836
vovilonn <vovadesign0792@gmail.com>
2022-06-03 00:04:18 +0300

    v2.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 import { formatEther } from "ethers/lib/utils";
+import { JsonFragment } from "@ethersproject/abi/src.ts/fragments";
 
 const walletRegExp = /^0x[a-fA-F0-9]{40}$/;
 
@@ -9,19 +10,21 @@ declare global {
     }
 }
 
-export type GetNonceCb = (walletSender: string, transactionNonce: number) => number | Promise<number>;
+export type CheckPaymentAvialability = (tx: ethers.providers.TransactionResponse) => boolean | Promise<boolean>;
+
+export type ErrorCode = "INVALID_ADDR" | "CONFLICT_ADDR" | "UNAVAILABLE";
 
 export interface VerifyPaymentResponse {
     sucess: boolean;
-    nonce: number;
-    receipt: ethers.providers.TransactionReceipt;
     transferAmount: number;
+    tx: ethers.providers.TransactionResponse;
+    receipt: ethers.providers.TransactionReceipt;
 }
 
 export interface Configuration {
     recipientWallet: string;
     rpcProviderUrl: string;
-    cb: GetNonceCb;
+    checkPaymentAvialability: CheckPaymentAvialability;
 }
 
 class _Error extends Error {
@@ -34,7 +37,7 @@ class _Error extends Error {
 }
 
 class ConfigurationError extends _Error {
-    constructor(message: string, errCode?: number) {
+    constructor(message: string, errCode?: ErrorCode) {
         super(ConfigurationError, message);
         if (errCode) {
             this.details = { errCode };
@@ -43,7 +46,7 @@ class ConfigurationError extends _Error {
 }
 
 class TransactionError extends _Error {
-    constructor(message: string, errCode?: number) {
+    constructor(message: string, errCode?: ErrorCode) {
         super(TransactionError, message);
         if (errCode) {
             this.details = { errCode };
@@ -52,52 +55,54 @@ class TransactionError extends _Error {
 }
 
 export class Web3Payments {
-    private static _configured: boolean = false;
-    private static _provider: ethers.providers.JsonRpcProvider;
-    private static _recipientWallet: string;
-    private static _getNonceByWalletSender: GetNonceCb;
+    private _provider: ethers.providers.JsonRpcProvider;
+    private _recipientWallet: string;
+    /**
+     * description: Fuction to check if a payment has already been made
+     */
+    private _checkPaymentAvialability: CheckPaymentAvialability;
 
-    private static checkConnection() {
-        if (!this._configured) {
-            throw new ConfigurationError("Service not configured", 4000);
-        }
-    }
-
-    public static configure({ recipientWallet, rpcProviderUrl, cb }: Configuration): void {
+    constructor({ recipientWallet, rpcProviderUrl, checkPaymentAvialability }: Configuration) {
         if (!walletRegExp.test(recipientWallet)) {
-            throw new ConfigurationError("The recipient's wallet is not a valid wallet address", 4001);
+            throw new ConfigurationError("The recipient's wallet is not a valid wallet address", "INVALID_ADDR");
         }
         this._recipientWallet = recipientWallet;
-        this._getNonceByWalletSender = cb;
+        this._checkPaymentAvialability = checkPaymentAvialability;
         this._provider = new ethers.providers.JsonRpcProvider(rpcProviderUrl);
-        this._configured = true;
     }
 
-    public static async verifyPayment(txHash: string): Promise<VerifyPaymentResponse> {
-        this.checkConnection();
-        const receipt = await this._provider.waitForTransaction(txHash, null, 10000);
+    public decodeTxData(
+        abi: string | readonly (string | ethers.utils.Fragment | JsonFragment)[],
+        data: string
+    ): ethers.utils.Result {
+        const iface = new ethers.utils.Interface(abi);
+        return iface.decodeFunctionData(data.slice(0, 10), data);
+    }
+
+    public async verifyPayment(txHash: string, timeout: number = 10000): Promise<VerifyPaymentResponse> {
+        const receipt = await this._provider.waitForTransaction(txHash, null, timeout);
         const tx = await this._provider.getTransaction(receipt.transactionHash);
         const transferAmount = +formatEther(tx.value);
 
         if (receipt.status === 0) {
-            return { sucess: false, nonce: tx.nonce, transferAmount, receipt };
+            return { sucess: false, transferAmount, receipt, tx };
         }
         if (receipt.status === 1) {
             if (tx.to.toLocaleLowerCase() !== this._recipientWallet.toLocaleLowerCase()) {
                 throw new TransactionError(
                     "The transfer was to a wallet that does not match the recipient wallet",
-                    5000
+                    "CONFLICT_ADDR"
                 );
             }
-            const lastPaymentNonce = await this._getNonceByWalletSender(tx.from, tx.nonce);
-            if (tx.nonce <= lastPaymentNonce) {
-                throw new TransactionError("Payment for this transaction has already been made", 5001);
+            const paymentIsAvialable = await this._checkPaymentAvialability(tx);
+            if (!paymentIsAvialable) {
+                throw new TransactionError("Payment for this transaction has already been made", "UNAVAILABLE");
             }
-            return { sucess: true, nonce: tx.nonce, transferAmount, receipt };
+            return { sucess: true, transferAmount, receipt, tx };
         }
     }
 
-    public static get recipientWallet() {
+    public get recipientWallet() {
         return this._recipientWallet;
     }
 }
