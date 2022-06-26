464b71998be360a49b68ee992e682b0c553c8b3e
vovilonn <vovadesign0792@gmail.com>
2022-06-26 20:29:22 +0300

    v3

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import { ethers } from "ethers";
 import { formatEther } from "ethers/lib/utils";
 import { JsonFragment } from "@ethersproject/abi/src.ts/fragments";
 
-const walletRegExp = /^0x[a-fA-F0-9]{40}$/;
-
 declare global {
     interface Error {
         details: any;
@@ -19,12 +17,14 @@ export interface VerifyPaymentResponse {
     transferAmount: number;
     tx: ethers.providers.TransactionResponse;
     receipt: ethers.providers.TransactionReceipt;
+    decodedData?: any;
 }
 
 export interface Configuration {
     recipientWallet: string;
     rpcProviderUrl: string;
     checkPaymentAvialability: CheckPaymentAvialability;
+    abi: string | readonly (string | ethers.utils.Fragment | JsonFragment)[];
 }
 
 class _Error extends Error {
@@ -57,25 +57,21 @@ class TransactionError extends _Error {
 export class Web3Payments {
     private _provider: ethers.providers.JsonRpcProvider;
     private _recipientWallet: string;
+    private _abi: string | readonly (string | ethers.utils.Fragment | JsonFragment)[];
     /**
      * description: Fuction to check if a payment has already been made
      */
     private _checkPaymentAvialability: CheckPaymentAvialability;
 
-    constructor({ recipientWallet, rpcProviderUrl, checkPaymentAvialability }: Configuration) {
-        if (!walletRegExp.test(recipientWallet)) {
-            throw new ConfigurationError("The recipient's wallet is not a valid wallet address", "INVALID_ADDR");
-        }
+    constructor({ recipientWallet, rpcProviderUrl, checkPaymentAvialability, abi }: Configuration) {
         this._recipientWallet = recipientWallet;
         this._checkPaymentAvialability = checkPaymentAvialability;
         this._provider = new ethers.providers.JsonRpcProvider(rpcProviderUrl);
+        this._abi = abi;
     }
 
-    public decodeTxData(
-        abi: string | readonly (string | ethers.utils.Fragment | JsonFragment)[],
-        data: string
-    ): ethers.utils.Result {
-        const iface = new ethers.utils.Interface(abi);
+    public decodeTxData(data: string): ethers.utils.Result {
+        const iface = new ethers.utils.Interface(this._abi);
         return iface.decodeFunctionData(data.slice(0, 10), data);
     }
 
@@ -88,7 +84,12 @@ export class Web3Payments {
             return { sucess: false, transferAmount, receipt, tx };
         }
         if (receipt.status === 1) {
-            if (tx.to.toLocaleLowerCase() !== this._recipientWallet.toLocaleLowerCase()) {
+            const decodedData = this.decodeTxData(tx.data);
+
+            if (
+                tx.to.toLowerCase() !== this._recipientWallet.toLowerCase() &&
+                decodedData?.recipient?.toLowerCase() !== this._recipientWallet.toLowerCase()
+            ) {
                 throw new TransactionError(
                     "The transfer was to a wallet that does not match the recipient wallet",
                     "CONFLICT_ADDR"
@@ -98,11 +99,15 @@ export class Web3Payments {
             if (!paymentIsAvialable) {
                 throw new TransactionError("Payment for this transaction has already been made", "UNAVAILABLE");
             }
-            return { sucess: true, transferAmount, receipt, tx };
+            return { sucess: true, transferAmount, receipt, tx, decodedData };
         }
     }
 
     public get recipientWallet() {
         return this._recipientWallet;
+    }
+
+    public get provider() {
+        return this._provider;
     }
 }
